5d26ca7f6154ad29920671bc13132263ae6ae2fb
fabritsiusda <fabritsiusda@gmail.com>
2020-11-04 16:42:15 +0500

    create first controllers, configure dependicies

---
+++ src/main/java/main/controllers/DefaultController.java
@@ -0,0 +1,14 @@
+package main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+
+}
